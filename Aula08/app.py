a5104ccff6128865925fb7e1a5fa9b4b365a0c84
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-18 08:11:00 -0300

    Aula 08 sobre if, else, operadores lógicos e de comparação finalizado!

---
+++ Aula08/app.py
@@ -0,0 +1,54 @@
+# tenho_sede = True
+# if tenho_sede:
+#     print("Beber Água.")
+# print("Vida que segue.")
+
+# esta_frio = True
+# if esta_frio:
+#     print("Vestir um casaco")
+# else:
+#     print("Vestir Camiseta")
+
+
+tenho_sede = True
+tenho_fome = True
+
+estou_em_dieta = True
+
+# if tenho_sede or tenho_fome:
+#     print("Vou na cozinha")
+# else:
+#     print("Ficar vendo Netflix")
+
+# if tenho_sede and tenho_fome:
+#     print("Fazer sanduiche com coca")
+# else:
+#     print("Não tenho fome ou não tenho sede")
+ 
+# if tenho_sede and tenho_fome:
+#     print("Fazer sanduiche com coca")
+# elif tenho_sede and not(tenho_fome):
+#     if estou_em_dieta:
+#         print("Tomar água!")
+#     else:
+#         print("Tomar uma coquinha")
+# elif not(tenho_sede) and tenho_fome:
+#     print("Fazer um sanduiche")
+# else:
+#     print("Ficar vendo netflix")
+
+# num1 = 5
+# num2 = 32
+
+# if num1 == num2:
+#     print("num1 é igual num2")
+# elif num1 != num2:
+#     print("num1 é diferente de num2")
+# elif num1 > num2:
+#     print("num1 é maior de num2")
+# elif num1 < num2:
+#     print("num1 é menor de num2")
+# elif num1 >= num2:
+#     print("num1 é maior ou igual que num2")
+# elif num1 <= num2:
+#     print("num1 é menor ou igual que num2")
